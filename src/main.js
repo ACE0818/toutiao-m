3c49b7a462ce44ba8c2d5400ab51b276c0cf4181
ACE0818 <2819558956@qq.com>
2023-11-01 23:23:00 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less'
+// 加载 Vant 核心组件库
+import Vant from 'vant'
+// 加载 Vant 全局样式
+import 'vant/lib/index.css'
+// 注册使用 Vant 组件库
+import 'amfe-flexible'
 
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
